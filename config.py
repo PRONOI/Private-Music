1dcb10ae9b82d47a2b1e72ab2d7d1e923f634b58
PRANAY <84889578+PRONOI@users.noreply.github.com>
2024-11-14 18:43:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,22 +6,22 @@ load_dotenv()
 get_queue = {}
 
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "4993786"))
 API_HASH = getenv("API_HASH")
 
-ASS_HANDLER = list(getenv("ASS_HANDLER", ".").split())
+ASS_HANDLER = list(getenv("ASS_HANDLER", "/").split())
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1063334882").split()))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
+PING_IMG = getenv("PING_IMG", "https://unitedcamps.in/Images/file_6694.jpg")
 START_IMG = getenv("START_IMG")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ALONExSUPPORT")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/itz_Xdpranay")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1063334882").split()))
